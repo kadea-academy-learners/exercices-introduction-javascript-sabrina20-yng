e96f740f9af4be76eda6292a7fbdde9258773bdf
sabrina20-yng <sabrinayanga20@gmail.com>
2025-10-16 20:30:25 +0200

    feat:final merge

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,14 @@
 // Exercice 7
 // Écrivez votre code ici
+let ageEnfant = 10;
+let agePere = ageEnfant *2 ;
+let ageMere = agePere - 5 ;
+let ageGrandPere = (ageMere *2) + (ageEnfant /2);
+let ageOncle = agePere + 10 ;
+
+// Affichage des resultats
+console.log ( "Age de l'enfant:" ,ageEnfant) ;
+console.log ("Age du pere :" , agePere) ;
+console.log ("Age de la mere :",ageMere) ;
+console.log ("Age du grand-pere :" ,ageGrandPere)
+console.log ("Age de l'oncle : " ,ageOncle )
